c012c1b3423b5cc945a8e9bc6baa13cb6cab449e
hannahbanana <hannahdk00@gmail.com>
2024-04-24 11:01:41 +0200

    pictures are displaying

--- index.php
+++ index.php
@@ -102,7 +102,7 @@
                         $brand_id=$row['brand_id'];
                         echo "<div class='col-md-4 mb-4'>
                         <div class='card' >
-                            <img src='./admin-area/product_images/$product_image1' class='card-img-top'>
+                            <img src='./admin_area/product_images/$product_image1' class='card-img-top'>
                             <div class='card-body'>
                                 <h5 class='card-title'>$product_title</h5>
                                 <p class='card-text'>$product_description</p>
